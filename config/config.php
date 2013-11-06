6bc0e74a94a2f7bdf3265e8f76ab1732758c822b
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-11-06 09:01:54 +0100

    Moved Number class over from Isotope project

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2013
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Backend widgets
+ */
+$GLOBALS['BE_FFL']['numberField'] = 'Haste\Number\BackendWidget';
