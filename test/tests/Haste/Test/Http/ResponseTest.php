6cdf794baeeb20a6ef5f6c78e0477a7666e384e4
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-11-11 20:26:13 +0100

    More tests :-)

--- test/tests/Haste/Test/Http/ResponseTest.php
+++ test/tests/Haste/Test/Http/ResponseTest.php
@@ -26,11 +26,34 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     {
         $objResponse = new Response('Foobar');
         $this->assertSame(200, $objResponse->getStatusCode());
+        $objResponse = new Response('Foobar', 500);
+        $this->assertSame(500, $objResponse->getStatusCode());
     }
 
-    public function testOutput()
+    public function testRegularOutput()
     {
         $objResponse = new Response('Foobar');
         $this->assertSame((string) $objResponse, "HTTP/1.1 200 OK\nContent-Type: text/plain; charset=utf-8\nContent-Length: 6\n\nFoobar");
+    }
+
+    public function testJsonOutput()
+    {
+        $objResponse = new JsonResponse(array('foo' => 'bar'), 201);
+        $this->assertSame(201, $objResponse->getStatusCode());
+        $this->assertSame((string) $objResponse, "HTTP/1.1 201 Created\nContent-Type: application/json; charset=utf-8\nContent-Length: 13\n\n{\"foo\":\"bar\"}");
+    }
+
+    public function testXmlOutput()
+    {
+        $objResponse = new XmlResponse('<foo><bar>indeed</bar></foo>', 304);
+        $this->assertSame(304, $objResponse->getStatusCode());
+        $this->assertSame((string) $objResponse, "HTTP/1.1 304 Not Modified\nContent-Type: application/xml; charset=utf-8\nContent-Length: 28\n\n<foo><bar>indeed</bar></foo>");
+    }
+
+    public function testHtmlOutput()
+    {
+        $objResponse = new HtmlResponse('<!DOCTYPE html><html lang="en"><head></head><body></body></html>', 100);
+        $this->assertSame(100, $objResponse->getStatusCode());
+        $this->assertSame((string) $objResponse, "HTTP/1.1 100 Continue\nContent-Type: text/html; charset=utf-8\nContent-Length: 64\n\n<!DOCTYPE html><html lang=\"en\"><head></head><body></body></html>");
     }
 }
