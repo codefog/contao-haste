0a85c15039b20f299affa07fec26a27c20a0e944
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-02-03 15:36:50 +0100

    Fixed options converter

--- library/Haste/Form/Form.php
+++ library/Haste/Form/Form.php
@@ -345,7 +345,7 @@ class Form extends \Controller
         $arrDca = $strClass::getAttributesFromDca($arrDca, $arrDca['name'], $arrDca['value']);
 
         // Convert optgroups so they work with FormSelectMenu
-        if (is_array($arrDca['options']) && !array_is_assoc($arrDca['options'])) {
+        if (is_array($arrDca['options']) && array_is_assoc($arrDca['options'])) {
             $arrOptions = $arrDca['options'];
             $arrDca['options'] = array();
 
