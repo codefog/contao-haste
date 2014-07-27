241a651f52826477c721b1f7bf904f05a0d2071c
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-07-27 21:51:08 +0200

    The data collection now supports iterating over the values

--- library/Haste/Data/Collection.php
+++ library/Haste/Data/Collection.php
@@ -13,11 +13,93 @@
 namespace Haste\Data;
 
 
-class Collection extends Plain
+class Collection implements \ArrayAccess, \IteratorAggregate
 {
+    /**
+     * @var array
+     */
+    protected $data;
+
 
     public function __construct(array $value, $label='', array $additional=array())
     {
-        parent::__construct($value, $label, $additional);
+        $this->data = array_merge(
+            array(
+                'value' => $value,
+                'label' => $label
+            ),
+            $additional
+        );
+    }
+
+    /**
+     * Get a string representation of the data collection
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $varValue = ($this->data['formatted'] ?: $this->data['value']);
+
+        if (is_array($varValue)) {
+            return implode('', $varValue);
+        }
+
+        return (string) $varValue;
+    }
+
+    /**
+     * Get array iterator on the value object
+     *
+     * @return \Traversable
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->data['value']);
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Retrieve array value
+     *
+     * @param mixed $offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    /**
+     * Set array value
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * Unset array value
+     *
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
     }
 }
