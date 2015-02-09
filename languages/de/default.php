3eaf761251975781adb05385dbffe609aa74bc36
Yanick Witschi <yanick.witschi@terminal42.ch>
2015-02-09 17:00:41 +0100

    Translated weitght units to german

--- languages/de/default.php
+++ languages/de/default.php
@@ -19,6 +19,25 @@
 
 
 /**
+ * Errors
+ */
+$GLOBALS['TL_LANG']['ERR']['numberInputNotAllowed'] = 'Bitte geben Sie eine valide Zahl ein (e.g. 5\'000, 5.00, 5,00, 5\'000.00)';
+
+/**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['HST']['advanced_filter'] = 'Erweiterte Filter:';
+$GLOBALS['TL_LANG']['HST']['advanced_filter']   = 'Erweiterte Filter:';
+
+/**
+ * Weight Units
+ * http://www.metric-conversions.org/weight/weight-conversions.htm
+ */
+$GLOBALS['TL_LANG']['WGT']['mg']                    = array('Milligramm (mg)', 'Eine Masseinheit; entspricht 1 Tausendstel Gramm.');
+$GLOBALS['TL_LANG']['WGT']['g']                     = array('Gramm (g)', 'Eine Masseinheit; entspricht 1 Tausendstel Kilogramm.');
+$GLOBALS['TL_LANG']['WGT']['kg']                    = array('Kilogramm (kg)', 'Ein Kilogramm entspricht 1\'000 Gramm oder 2.2 Pfund; Entspricht dem Gewicht von einem Liter Wasser.');
+$GLOBALS['TL_LANG']['WGT']['t']                     = array('Tonne (t)', 'Eine Tonne entspricht 1\'000 Kilogramm, oder 2,204.6 Pfund.');
+$GLOBALS['TL_LANG']['WGT']['ct']                    = array('Karat (ct)', 'Das metrische Karat ist eine Masseinheit für die Masse von Edelsteinen. 1 metrisches Karat = 0,2 g = 2 · 10−4 kg.');
+$GLOBALS['TL_LANG']['WGT']['oz']                    = array('Unze (oz)', 'Entspricht einem Sechzehntel eines Pfunds oder 28.35 Gramm.');
+$GLOBALS['TL_LANG']['WGT']['lb']                    = array('Pfund (lb)', 'Eine Masseinheit; entspricht 16 Unzen');
+$GLOBALS['TL_LANG']['WGT']['st']                    = array('Stone (st)', 'Ein britische Masseinheit; Entspricht 14 Pfund.');
+$GLOBALS['TL_LANG']['WGT']['grain']                 = array('Grain', 'Eine Masseinheit; entspricht 1/7000 Pfund bzw. einem Troy Grain oder 64.799 Milligramm.');
