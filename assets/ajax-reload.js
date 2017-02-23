c5e24bfe5eda418f69596c747501e416ed3f957a
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-02-23 11:47:09 +0100

    Restructured the JS file

--- assets/ajax-reload.js
+++ assets/ajax-reload.js
@@ -1,11 +1,11 @@
-window.HasteAjaxReload = {
-    elementsInProgress: {},
-    eventsInProgress: {},
+(function () {
+    var elementsInProgress = {};
+    var eventsInProgress = {};
 
     /**
      * Dispatch the events
      */
-    dispatchEvents: function () {
+    function dispatchEvents() {
         if (arguments.length < 1) {
             console.error('Please provide at least one event');
         }
@@ -31,27 +31,26 @@ window.HasteAjaxReload = {
         });
 
         if (Object.keys(els).length > 0 && events.length > 0) {
-            this._sendRequest(els, events);
+            sendRequest(els, events);
         }
-    },
+    }
 
     /**
      * Send the request
      * @param {Object} els
      * @param {Array} events
-     * @private
      */
-    _sendRequest: function (els, events) {
-        els = this._filterElementsInProgress(els);
-        events = this._filterEventsInProgress(events);
+    function sendRequest(els, events) {
+        els = filterElementsInProgress(els);
+        events = filterEventsInProgress(events);
 
         // Return if everything is in progress and there is nothing to update
         if (Object.keys(els).length < 1 || events.length < 1) {
             return;
         }
 
-        this._toggleElementsInProgress(els, true);
-        this._toggleEventsInProgress(events, true);
+        toggleElementsInProgress(els, true);
+        toggleEventsInProgress(events, true);
 
         // Add the CSS class
         for (var key in els) {
@@ -76,65 +75,65 @@ window.HasteAjaxReload = {
                 console.error(xhr);
             }
 
-            this._toggleElementsInProgress(els, false);
-            this._toggleEventsInProgress(events, false);
-        }.bind(this);
+            toggleElementsInProgress(els, false);
+            toggleEventsInProgress(events, false);
+        };
 
         xhr.send();
-    },
+    }
 
     /**
      * Filter elements in progress
      * @param {Object} els
      * @return {Object}
-     * @private
      */
-    _filterElementsInProgress: function (els) {
+    function filterElementsInProgress(els) {
         var filtered = {};
 
         for (var key in els) {
-            if (!this.elementsInProgress[key]) {
+            if (!elementsInProgress[key]) {
                 filtered[key] = els[key];
             }
         }
 
         return filtered;
-    },
+    }
 
     /**
      * Toggle elements in progress
      * @param {Object} els
      * @param {Boolean} status
-     * @private
      */
-    _toggleElementsInProgress: function (els, status) {
+    function toggleElementsInProgress(els, status) {
         for (var key in els) {
-            this.elementsInProgress[key] = status;
+            elementsInProgress[key] = status;
         }
-    },
+    }
 
     /**
      * Filter the events in progress
      * @param {Array} events
      * @return {Array}
-     * @private
      */
-    _filterEventsInProgress: function (events) {
+    function filterEventsInProgress(events) {
         return events.filter(function (event) {
-            return !this.eventsInProgress[event];
-        }.bind(this));
-    },
+            return !eventsInProgress[event];
+        });
+    }
 
     /**
      * Toggle events in progress
      * @param {Array} events
      * @param {Boolean} status
-     * @private
      */
-    _toggleEventsInProgress: function (events, status) {
+    function toggleEventsInProgress(events, status) {
         events.forEach(function (event) {
-            this.eventsInProgress[event] = status;
-        }.bind(this));
+            eventsInProgress[event] = status;
+        });
     }
-};
 
+    // Public API
+    window.HasteAjaxReload = {
+        dispatchEvents: dispatchEvents
+    };
+})();
