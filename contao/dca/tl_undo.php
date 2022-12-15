30cdd9b2fb4cc83dd8a45cc62951b527f8a1ae56
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-12-15 08:24:13 +0100

    Do not show haste_data in tl_undo

--- contao/dca/tl_undo.php
+++ contao/dca/tl_undo.php
@@ -5,5 +5,6 @@ $GLOBALS['TL_DCA']['tl_undo']['list']['operations']['undo']['button_callback'] =
 
 // Add fields to tl_undo
 $GLOBALS['TL_DCA']['tl_undo']['fields']['haste_data'] = [
+    'eval' => ['doNotShow' => true],
     'sql' => ['type' => 'blob', 'notnull' => false],
 ];
